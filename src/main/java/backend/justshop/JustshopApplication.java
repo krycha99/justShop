9a3b59c1614c3459b81807ca1413aa8eab78740d
Krystian Sudoł <45632898+krycha99@users.noreply.github.com>
2022-11-14 16:26:47 +0100

    uploading files

---
+++ src/main/java/backend/justshop/JustshopApplication.java
@@ -0,0 +1,13 @@
+package backend.justshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustshopApplication.class, args);
+	}
+
+}
