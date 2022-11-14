8a64f0a83de775acf6be43f8746e7550d6363961
Krystian Sudoł <45632898+krycha99@users.noreply.github.com>
2022-11-14 16:35:43 +0100

    git test

--- src/main/java/backend/justshop/JustshopApplication.java
+++ src/main/java/backend/justshop/JustshopApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JustshopApplication {
-
+//test
 	public static void main(String[] args) {
 		SpringApplication.run(JustshopApplication.class, args);
 	}
