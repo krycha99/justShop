0242a699d63c36f9806dff6dd2a75c4ea39e3504
Krystian Sudoł <45632898+krycha99@users.noreply.github.com>
2022-11-16 14:07:27 +0100

    user registration and security config

--- src/main/java/backend/justshop/JustshopApplication.java
+++ src/main/java/backend/justshop/JustshopApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JustshopApplication {
-//test
 	public static void main(String[] args) {
 		SpringApplication.run(JustshopApplication.class, args);
 	}
---
+++ src/main/java/backend/justshop/user/UserRole.java
@@ -0,0 +1,6 @@
+package backend.justshop.user;
+
+public enum UserRole {
+    USER,
+    ADMIN
+}
